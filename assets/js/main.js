cd1b78f7b6c599fe4c68ad5cff94944e4bc89c4a
ayesha <aysh905@gmail.com>
2018-03-06 07:31:05 +0500

    Slider Updated.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,19 @@
 $(function(){
-    var carouselWidth = $(".owl-carousel").width()-30;
-    
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        items: 4,
-        nav: true,
-        dots: false,
-        callbacks: true,
-        itemsScaleUp: true,
-        navText: ["<div class='arrow'><img src='assets/images/left-arrow.png' alt='left arrow'/></div>","<div class='arrow'><img src='assets/images/right-arrow.png' alt='right arrow'/></div>"],
-        onChanged:function(){
-            //add transform for 2nd active slide
-            $(".owl-item.active").eq(1).css({width: (carouselWidth/10)*3.1});
-            $(".owl-item.active").eq(2).css({width: (carouselWidth/10)*2.3});
-            $(".owl-item.active").eq(3).css({width: (carouselWidth/10)*2.3});
-            $(".owl-item.active").eq(4).css({width: (carouselWidth/10)*2.3});
-        },
-        //set init transform
-        onInitialized:function(){
 
-            $(".owl-item.active").eq(0).css({width: (carouselWidth/10)*3.1});
-            $(".owl-item.active").eq(1).css({width: (carouselWidth/10)*2.3});
-            $(".owl-item.active").eq(2).css({width: (carouselWidth/10)*2.3});
-            $(".owl-item.active").eq(3).css({width: (carouselWidth/10)*2.3});
-        },
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:4
-            }
-        }
+    $('.slick').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        prevArrow: "<div class='arrow prev'><img src='assets/images/left-arrow.png' alt='left arrow'/></div>",
+        nextArrow: "<div class='arrow next'><img src='assets/images/right-arrow.png' alt='right arrow'/></div>"
+    });
+
+    $('.slick').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        var hide = currentSlide+1
+        var show = nextSlide+1;
+        $(".text-content"+hide).hide();
+        $(".text-content"+show).show();
     });
 
     $("#bar-toggle").on("click", function(){
